cd65bc1faea057be5a7c74f0fa44caec66ce418e
Ruck-us <alvinrucker@emailline.net>
2025-03-30 12:49:44 -0700

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, World! I am an alert box!");
+
+function My_First_Function() {
+    var String = "Kiss me, I'm Irish!";
+    var result = "<span style='color:green'>" + String + "</span>";
+    document.getElementById("Irish").innerHTML = result; 
+    
+}
